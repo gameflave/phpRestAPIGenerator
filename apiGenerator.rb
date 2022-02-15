4fe9408ad890c7918376a0a8bacf9b689cfe8f69
flave <32307170+gameflave@users.noreply.github.com>
2022-02-15 10:31:13 +0100

    typo

--- apiGenerator.rb
+++ apiGenerator.rb
@@ -36,7 +36,7 @@ module API_Generator
         fileContent.concat(GenerateSelectAllFonction(table))
         fileContent.concat(GenerateInsertFonction(table, db.getCols(table)))
         fileContent.concat(GenerateUpdateFonction(table, db.getCols(table)))
-        fileContent.concat(GenerateDeleteFonction(table, db.getPrimaryCols(table)))7
+        fileContent.concat(GenerateDeleteFonction(table, db.getPrimaryCols(table)))
         fileContent.concat("}")
         
         file = File.new(path + table + ".php", "w+")
